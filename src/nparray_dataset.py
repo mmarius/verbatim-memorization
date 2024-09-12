7a2cf3bb0ca6c52090036fa13f529e9be8323cd4
mmarius <m_marius@online.de>
2024-09-12 13:25:44 -0400

    formatting

--- src/nparray_dataset.py
+++ src/nparray_dataset.py
@@ -10,7 +10,7 @@ class NumpyArrayDataset(torch.utils.data.Dataset):
     def __init__(
         self,
         data,
-        sample_range,
+        sample_range=None,
         inject_data=None,
         inject_every_n=None,
         tokenizer=None,
@@ -19,9 +19,17 @@ class NumpyArrayDataset(torch.utils.data.Dataset):
     ):
         super(NumpyArrayDataset, self).__init__()
         self.window_size = 256 if "window_size" not in kwargs else kwargs["window_size"]
-        self.data = data[sample_range[0] : sample_range[1], : self.window_size + 64]
+        if sample_range:
+            print(f"Loading data from {sample_range[0]} to {sample_range[1]}")
+            # we use a subset of the data
+            self.data = data[sample_range[0] : sample_range[1], : self.window_size + 64]
+        else:
+            # we use all of the data
+            self.data = data[:, : self.window_size + 64]
         self.counters = collections.defaultdict(int)
-        self.inject_data = inject_data
+        self.inject_data = (
+            inject_data  # dict mapping from integers (sample ids) to strings (the text)
+        )
         self.inject_every_n = inject_every_n
         if inject_data and tokenizer is None:
             raise ValueError
@@ -33,19 +41,25 @@ class NumpyArrayDataset(torch.utils.data.Dataset):
     def __getitem__(self, index):
         # print_with_rank(self.debug_id, index)
         if self.inject_data:
-            for key in self.inject_data:
-                if self.data.shape[0] > 10_000 and index % self.inject_every_n == key:
-                    self.debug_counters[f"counter-{key}"].append(index)
-                    # print_with_rank(self.debug_id, key, self.debug_counters)
-                    return {
-                        "input_ids": self.tokenizer(
-                            self.inject_data[key],
-                            return_tensors="pt",
-                            truncation=True,
-                            padding="max_length",
-                            max_length=self.window_size + 1,
-                        ).input_ids[0]
-                    }
+            for key in self.inject_data:  # key is the id of the sample to inject
+                # we only inject data if we have more normal samples than the injection frequency
+                # otherwise we end up training only on the injected data
+                if self.data.shape[0] > 10_000:
+                    # inject_every_n is 10_000 or 40_000
+                    if index % self.inject_every_n == key:
+                        self.debug_counters[f"counter-{key}"].append(index)
+                        # print_with_rank(self.debug_id, key, self.debug_counters)
+                        # return one of the injection sequences
+                        return {
+                            "input_ids": self.tokenizer(
+                                self.inject_data[key],
+                                return_tensors="pt",
+                                truncation=True,
+                                padding="max_length",
+                                max_length=self.window_size + 1,
+                            ).input_ids[0]
+                        }
+        # return a normal example
         return {
             "input_ids": torch.tensor(
                 self.data[index, : self.window_size + 1].astype(np.int64)
